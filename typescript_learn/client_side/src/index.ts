4073d3bb169430f7018c63f9da1a2b5e3fdcf651
lc107 <2150392886>
2020-08-09 11:10:37 +0800

    删除文件夹

--- typescript_learn/client_side/src/index.ts
+++
@@ -1,6 +0,0 @@
-// import './example/basic-type'
-// import './example/symbol'
-// import './example/interface'
-
-// import './example/OtherBloggers/WeChatOfficialAccount'
-import './example/ebook/wechartEbook'
